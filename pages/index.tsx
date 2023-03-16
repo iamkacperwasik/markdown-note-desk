9778266f682692b8f70c787602e507a698b99ba6
Kacper Wąsik <kacper@wasik.me>
2023-03-16 14:43:41 +0100

    init

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+const Home = () => (
+  <>
+    <Head>
+      <meta name="robots" content="noindex" />
+    </Head>
+
+    <div>
+      <h1>Hello</h1>
+    </div>
+  </>
+)
+
+export default Home
