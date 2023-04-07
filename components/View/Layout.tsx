69837adbaac4f69fb9f121b382633d7173a5be2f
Kacper Wąsik <kacper@wasik.me>
2023-04-07 21:48:44 +0200

    move sidebar to View/

--- components/View/Layout.tsx
+++ components/View/Layout.tsx
@@ -1,10 +1,10 @@
 import {When} from "react-if"
 
-import {Sidebar} from "components/Panel/Sidebar"
 import {DeletingPanel} from "components/Panel/State/DeletingPanel"
 import {EditingPanel} from "components/Panel/State/EditingPanel"
 import {ViewingPanel} from "components/Panel/State/ViewingPanel"
 import {MarkdownPreview} from "components/View/MarkdownPreview"
+import {Sidebar} from "components/View/Sidebar"
 
 import {useNote} from "hooks/useNote"
 
