1f8c975115c89dbf49634749db74f1322d773cc6
Kacper Wąsik <kacper@wasik.me>
2023-04-07 21:22:20 +0200

    fix noteCount

--- utils/supabase/fetchNotesCount.ts
+++ utils/supabase/fetchNotesCount.ts
@@ -6,11 +6,10 @@ export const fetchNotesCount = async (
   supabase: SupabaseClient<Database>,
   userId: string
 ): Promise<number> => {
-  const {data: notesCount} = await supabase
+  const {count: notesCount} = await supabase
     .from("notes")
     .select("*", {count: "exact", head: true})
     .eq("userId", userId)
 
-  //@ts-ignore
-  return notesCount
+  return notesCount!
 }
